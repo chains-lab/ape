551cccf2cc5bde95264ad1ee1eff2f0a50ffdf02
trpdjke <yehorushakov@gmail.com>
2025-07-30 02:18:20 +0300

    first commit

---
+++ error.go
@@ -0,0 +1,48 @@
+package apperr
+
+import (
+	"errors"
+
+	"google.golang.org/grpc/codes"
+	"google.golang.org/protobuf/protoadapt"
+)
+
+type ErrorObject struct {
+	code    codes.Code
+	reason  string
+	message string
+	details []protoadapt.MessageV1
+	cause   error
+}
+
+func (e *ErrorObject) Error() string {
+	return e.message
+}
+
+func (e *ErrorObject) Unwrap() error {
+	return e.cause
+}
+
+func (e *ErrorObject) Is(target error) bool {
+	var be *ErrorObject
+	if errors.As(target, &be) {
+		return e.reason == be.reason
+	}
+	return false
+}
+
+func (e *ErrorObject) Reason() string {
+	return e.reason
+}
+
+func (e *ErrorObject) Details() []protoadapt.MessageV1 {
+	if e.details == nil {
+		return nil
+	}
+
+	return e.details
+}
+
+func (e *ErrorObject) Code() codes.Code {
+	return e.code
+}
---
+++ go.mod
@@ -0,0 +1,10 @@
+module github.com/chains-lab/ape
+
+go 1.24.4
+
+require (
+	google.golang.org/grpc v1.74.2
+	google.golang.org/protobuf v1.36.6
+)
+
+require golang.org/x/sys v0.33.0 // indirect
