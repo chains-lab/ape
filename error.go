7154c702750aba36c8791d666e7109a33efb7dca
trpdjke <yehorushakov@gmail.com>
2025-07-30 12:08:14 +0300

    delere NewError

--- error.go
+++ error.go
@@ -7,52 +7,26 @@ import (
 	"google.golang.org/protobuf/protoadapt"
 )
 
-func NewError(cause error, reason, message string, code codes.Code, details ...protoadapt.MessageV1) *ErrorObject {
-	return &ErrorObject{
-		code:    code,
-		reason:  reason,
-		message: message,
-		cause:   cause,
-		details: details,
-	}
-}
-
 type ErrorObject struct {
-	code    codes.Code
-	reason  string
-	message string
-	details []protoadapt.MessageV1
-	cause   error
+	Cause   error
+	Reason  string
+	Message string
+	Code    codes.Code
+	Details []protoadapt.MessageV1
 }
 
 func (e *ErrorObject) Error() string {
-	return e.message
+	return e.Message
 }
 
 func (e *ErrorObject) Unwrap() error {
-	return e.cause
+	return e.Cause
 }
 
 func (e *ErrorObject) Is(target error) bool {
 	var be *ErrorObject
 	if errors.As(target, &be) {
-		return e.reason == be.reason
+		return e.Reason == be.Reason
 	}
 	return false
-}
-
-func (e *ErrorObject) Reason() string {
-	return e.reason
-}
-
-func (e *ErrorObject) Details() []protoadapt.MessageV1 {
-	if e.details == nil {
-		return nil
-	}
-
-	return e.details
-}
-
-func (e *ErrorObject) Code() codes.Code {
-	return e.code
 }
