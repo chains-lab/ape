6ed8c5805cba9d37cc2414632ef0492b1324e84b
webzavet <yehorushakov@gmail.com>
2025-08-03 01:11:56 +0300

    add description for ErrorObject

--- error.go
+++ error.go
@@ -8,10 +8,20 @@ import (
 )
 
 type ErrorObject struct {
-	Cause   error
-	Reason  string
+	// Cause internal error search for which an error occurred
+	Cause error
+
+	// Reason is a unique identifier for the error type
+	Reason string
+
+	// Message  is a short, human-readable summary of the problem that SHOULD NOT change from occurrence to
+	// occurrence of the problem, except for purposes of localization (analogue title in json api).
 	Message string
-	Code    codes.Code
+
+	// Code is a gRPC status code that indicates the type of error
+	Code codes.Code
+
+	// Details is a list of additional details about the error, which can be used to provide more context
 	Details []protoadapt.MessageV1
 }
 
